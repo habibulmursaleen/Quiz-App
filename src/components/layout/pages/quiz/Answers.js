bf81e5261d2be322fb288f2359ccb505200f3c5c
habibulmursaleen <habibulmursaleen97@gmail.com>
2022-11-14 12:55:32 +0100

    quiz page

--- src/components/layout/pages/quiz/Answers.js
+++ src/components/layout/pages/quiz/Answers.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { Fragment } from "react";
 import CheckBox from "../../../forms/CheckBox";
 
 import classes from "../../../../styles/Answers.module.css";
 
-const Answers = ({ options = [], handleChange }) => {
+const Answers = ({ options = [], handleChange, input }) => {
   return (
     <div className={classes.answers}>
       {options.map((option, index) => (
-        <CheckBox
-          type="checkbox"
-          className={classes.answer}
-          text={option.title}
-          value={index}
-          checked={options.checked}
-          key={index}
-          onChange={(event) => handleChange(event, index)}
-        />
+        <Fragment key={index}>
+          {input ? (
+            <CheckBox
+              type="checkbox"
+              className={classes.answer}
+              text={option.title}
+              value={index}
+              checked={option.checked}
+              key={index}
+              onChange={(event) => handleChange(event, index)}
+            />
+          ) : (
+            <CheckBox
+              className={`${classes.answer} ${
+                option.correct
+                  ? classes.correct
+                  : option.checked
+                  ? classes.wrong
+                  : null
+              }`}
+              text={option.title}
+              type="checkbox"
+              defaultChecked={option.checked} //readonly// uncontrolled
+              key={index}
+              disabled
+            />
+          )}
+        </Fragment>
       ))}
     </div>
   );
